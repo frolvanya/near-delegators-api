66a679bd51e850cf799a06ddfad078b447e0078d
Ivan Frolov <frolvanya@gmail.com>
2024-02-06 20:57:52 -0500

    chore: removed useless file

--- .fly/scripts/1_storage_init.sh
+++
@@ -1,7 +0,0 @@
-FOLDER=/
-if [ ! -d "$FOLDER" ]; then
-    echo "$FOLDER is not a directory, copying storage_ content to storage"
-    cp -r /_. /
-    echo "deleting storage_..."
-    rm -rf /_
-fi
